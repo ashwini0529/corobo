bed50bf63453312200ba05b67c3243f23e5b58a6
Meet Mangukiya <meetmangukiya98@gmail.com>
2017-07-29 20:49:23 +0530

    answers/service: Add update endpoint
    `update` endpoint updates the coala repository with remote if repo
    exists, else clones it.
    
    Closes https://github.com/coala/corobo/issues/236

--- answers/service.py
+++ answers/service.py
@@ -3,11 +3,25 @@ import os
 
 from flask import Flask, jsonify, request
 from gensim.summarization import summarize
+import git
 
 from .final import get_answer, construct_graph
 from .extraction import parse_docs
+from .utils import get_abs_path
 
 app = Flask(__name__)
+
+
+@app.route('/update')
+def update_docs():
+    if 'coala' in os.listdir(get_abs_path('')):
+        git.Repo(get_abs_path('coala')).remote('origin').update()
+    else:
+        git.Repo.clone_from('https://github.com/coala/coala.git',
+                            get_abs_path('coala'))
+
+
+update_docs()
 
 DATA = parse_docs()
 GRAPH = construct_graph(DATA)
